d1c9d7afe7c778c37c160932443ff63dc18d5034
Siraz Naorem <siraz.naorem@neptune-ubi.com>
2023-11-14 17:48:24 +0530

    FE-01: [feat]: adding dummy dashboard

--- zerotha/src/app/app.module.ts
+++ zerotha/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewDashboardComponent } from 'src/components/view-dashboard/view-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewDashboardComponent
   ],
   imports: [
     BrowserModule,
