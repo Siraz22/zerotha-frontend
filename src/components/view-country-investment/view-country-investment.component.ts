b10f293606d5cbf946fe6b20a6e9c5c90972b8bc
Siraz22 <siraznaorem07@gmail.com>
2023-12-16 23:26:19 +0530

    FE-05 [feat]: adding basic searchParams for keeping track of activeTab

--- src/components/view-country-investment/view-country-investment.component.ts
+++ src/components/view-country-investment/view-country-investment.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { combineLatest, switchMap } from 'rxjs';
 
 import { CountryDTO } from '../../dto/CountryDTO';
 import { InvestmentType } from '../../enums/Investment-type';
 import { CountryService } from '../../service/country.service';
+
+interface CountryInvestmentQueryParams {
+    activeTab: string;
+}
 
 @Component({
     selector: 'app-view-country-investment',
@@ -16,23 +20,52 @@ export class ViewCountryInvestmentComponent implements OnInit {
     constructor(private route: ActivatedRoute, private router: Router, private countryService: CountryService) {}
 
     public currentCountryDTO: CountryDTO;
-    public activeTab: string = 'Insights';
-    public selectedInvestmentType: InvestmentType;
 
+    public selectedInvestmentType: InvestmentType;
     public InvestmentType = InvestmentType;
 
-    ngOnInit() {
-        this.route.parent.params.pipe(switchMap((params) => this.countryService.findOne(Number(params['countryId'])))).subscribe((countryDTO: CountryDTO) => {
-            this.currentCountryDTO = countryDTO;
-        });
+    public searchParams: CountryInvestmentQueryParams = { activeTab: '' };
 
-        this.route.data.subscribe((routeData) => {
-            this.selectedInvestmentType = routeData['selectedInvestmentType'];
-        });
+    ngOnInit() {
+        combineLatest([this.route.parent.params.pipe(switchMap((params) => this.countryService.findOne(Number(params['countryId'])))), this.route.data]).subscribe(
+            ([countryDTO, routeData]) => {
+                this.currentCountryDTO = countryDTO;
+                this.selectedInvestmentType = routeData['selectedInvestmentType'];
+                this.populateQueryParams();
+            }
+        );
     }
 
     public tabClicked(selectedTab: MatTabChangeEvent): void {
-        this.activeTab = selectedTab.tab.textLabel;
-        this.router.navigate([], { queryParams: { activeTab: this.activeTab } });
+        this.searchParams.activeTab = selectedTab.tab.textLabel;
+        this.setRouteQueryParams();
+    }
+
+    public getActiveTabIndex(): number {
+        switch (this.searchParams.activeTab) {
+            case 'Insights':
+                return 0;
+            case 'Holdings':
+                return 1;
+            case 'Orders':
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    //Use to populate Query Params
+    private populateQueryParams(): void {
+        this.route.queryParams.subscribe((queryParams) => {
+            this.searchParams.activeTab = queryParams['activeTab'];
+        });
+        this.setRouteQueryParams();
+    }
+
+    //Use to navigate to the route with Query Params
+    private setRouteQueryParams(): void {
+        this.router.navigate([], {
+            queryParams: this.searchParams,
+        });
     }
 }
