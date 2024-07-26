67fe8a3fd9b5cede82e3d483424c98b56a915aae
Siraz22 <siraznaorem07@gmail.com>
2024-07-26 23:39:53 +0530

    update environment.production values

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://zerotha-backend.onrender.com/api/v1',
-    API_ENDPOINT: '',
+    API_ENDPOINT: 'https://zerotha-backend.onrender.com/api/v1',
+    ALPACA_KEY: 'PK1O4HNXXOX7D8V5PPQI',
+    ALPACA_SECRET: 'G8knEQ4rc7KTNqEFzGuaiZ8oYwrS8uImD5LQ8aJn',
 };
