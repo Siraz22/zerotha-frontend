9b0e71dd0c9a5372dcdb28e724c99db5a31b6a2d
Siraz22 <siraznaorem07@gmail.com>
2024-07-26 23:45:50 +0530

    update

--- mynode.js
+++ mynode.js
@@ -5,10 +5,10 @@ const checkSign = '\u{2705}';
 const dotenv = require('dotenv').config({path: 'src/.env'}); ;
 
 const envFile = `export const environment = {
-    production: true,
-    API_ENDPOINT: '${process.env.API_ENDPOINT}',
-    ALPACA_KEY: '${process.env.ALPACA_KEY}',
-    ALPACA_SECRET: '${process.env.ALPACA_SECRET}',
+    production:true,
+    API_ENDPOINT:'${process.env.API_ENDPOINT}',
+    ALPACA_KEY:'${process.env.ALPACA_KEY}',
+    ALPACA_SECRET:'${process.env.ALPACA_SECRET}',
 };
 `;
 const targetPath = path.join(__dirname, './src/environments/environment.production.ts');
