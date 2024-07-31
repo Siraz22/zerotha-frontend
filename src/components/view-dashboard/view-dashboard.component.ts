31aab212ac2d6088bc05aab7bee9c350dc43597a
Siraz Naorem <siraznaorem07@gmail.com>
2024-07-31 22:54:09 +0530

    adding explicit button to go to country!

--- src/components/view-dashboard/view-dashboard.component.ts
+++ src/components/view-dashboard/view-dashboard.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-view-dashboard',
-  templateUrl: './view-dashboard.component.html',
-  styleUrls: ['./view-dashboard.component.css']
+    selector: 'app-view-dashboard',
+    templateUrl: './view-dashboard.component.html',
+    styleUrls: ['./view-dashboard.component.css'],
 })
 export class ViewDashboardComponent implements OnInit {
+    constructor(private router: Router) {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    public redirectToCountries(): void {
+        this.router.navigate(['/country']);
+    }
 }
